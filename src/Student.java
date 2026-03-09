b3ad637890f2fe78aafc4d583caf93bfa34a4a26
Clauu6 <martaaclaudiaa2@gmail.com>
2026-03-09 11:01:59 +0200

    c

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    int numarMatricol;
+    String prenume;
+    String nume;
+    String formatiedeStudiu;
+
+    public Student(int numarMatricol, String prenume, String nume, String formatiedeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatiedeStudiu = formatiedeStudiu;
+    }
+
+
+}
---
+++ src/ro/ulbs/proiectaresoftware/students/Application.java
@@ -0,0 +1,18 @@
+package ro.ulbs.proiectaresoftware.students;
+
+
+public class Application {
+    static void main() {
+        Student1 s1=new  Student1(112,"Ioan","Popa","TI21/1");
+        Student1 s2=new  Student1(112,"Maria","Oprea","TI21/1");
+        Student1 s3=new  Student1(120,"Alis","Popa","TI21/2");
+        Student1 s4=new  Student1(122,"Mihai","Vercedea","TI22/1");
+        Student1 s5=new  Student1(122,"Eugen","Uritescu","TI22/2");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+    }
+
+}
