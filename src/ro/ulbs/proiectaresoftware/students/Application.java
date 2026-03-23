b1b7fa5178448418d1aecc95bb7d6291e53f6695
Clauu6 <martaaclaudiaa2@gmail.com>
2026-03-23 11:17:46 +0200

    colectii

--- src/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,18 +1,70 @@
 package ro.ulbs.proiectaresoftware.students;
 
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Application {
+    public static boolean prezent(Student student, List<Student> studenti)
+    {
+/*
+        //boolean gasit;
+        if(student != studenti )
+
+           return false;
+
+return true;
+*/
+
+ return studenti.contains(student);
+
+    }
+    public static boolean prezentset(Student student, Set<Student> set)
+    {
+
+        return set.contains(student);
+
+    }
+
     static void main() {
-        Student1 s1=new  Student1(112,"Ioan","Popa","TI21/1");
-        Student1 s2=new  Student1(112,"Maria","Oprea","TI21/1");
-        Student1 s3=new  Student1(120,"Alis","Popa","TI21/2");
-        Student1 s4=new  Student1(122,"Mihai","Vercedea","TI22/1");
-        Student1 s5=new  Student1(122,"Eugen","Uritescu","TI22/2");
+        List<Student> studenti=new ArrayList();
+        System.out.println("nr de studenti din lista este : "+ studenti.size());
+        Student s1=new Student("112","Ioan","Popa","TI21/1");
+        Student s2=new Student("112","Maria","Oprea","TI21/1");
+        Student s3=new Student("120","Alis","Popa","TI21/2");
+        Student s4=new Student("122","Mihai","Vercedea","TI22/1");
+        Student s5=new Student("122","Eugen","Uritescu","TI22/2");
+
+        studenti.add(s1);
+        studenti.add(s2);
+        studenti.add(s3);
+        studenti.add(s4);
+        studenti.add(s5);
+
+        System.out.println("nr de studenti din lista este : "+ studenti.size());
+        System.out.println("Este prezent ?");
+        System.out.println(prezent(new Student("112","Ioan","Popa","TI21/1"),studenti));
+
+        Set<Student> set=new HashSet<>(studenti);
+        System.out.println(prezentset(new Student("112","Ioan","Popa","TI21/1"),set));
+/*
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
         System.out.println(s4);
         System.out.println(s5);
+*/
+
+
+        afiseaza(studenti);
+
+    }
+
+    private static void afiseaza(List<Student> studenti) {
+        for(Student s: studenti)  // este un iterator
+            System.out.println(s);
     }
 
 }
---
+++ src/ro/ulbs/proiectaresoftware/students/Student.java
@@ -0,0 +1,53 @@
+package ro.ulbs.proiectaresoftware.students;
+
+import java.util.Objects;
+
+public class Student {
+
+        String numarMatricol;
+        String prenume;
+        String nume;
+        String formatiedeStudiu;
+
+        public Student(String numarMatricol, String prenume, String nume, String formatiedeStudiu) {
+            this.numarMatricol = numarMatricol;
+            this.prenume = prenume;
+            this.nume = nume;
+            this.formatiedeStudiu = formatiedeStudiu;
+        }
+
+    public String getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getFormatiedeStudiu() {
+        return formatiedeStudiu;
+    }
+
+    @Override
+        public String toString(){
+         return String.format("%01d %10s %10s %10s",numarMatricol,prenume,nume,formatiedeStudiu);
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(prenume, student.prenume) && Objects.equals(nume, student.nume) && Objects.equals(formatiedeStudiu, student.formatiedeStudiu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(prenume, nume, formatiedeStudiu);
+    }
+}
+
+
